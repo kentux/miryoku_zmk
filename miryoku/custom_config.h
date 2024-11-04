64bcfc3fcd7993afdcf1279087baa3243fd32b81
quentin.fourel <quentin@fourel.eu>
2024-11-03 23:00:45 -0500

    feat: e_caret

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,15 +1,13 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-/ {
-    macros {
-        e_caret: e_caret {
-            wait-ms = <40>;
-            tap-ms = <40>;
-            bindings
-                = <&kp CARET>
-                , <&kp E>
-                ;
-        };
+macros {
+    e_caret: e_caret {
+        wait-ms = <40>;
+        tap-ms = <40>;
+        bindings
+            = <&kp CARET>
+            , <&kp E>
+            ;
     };
 };
